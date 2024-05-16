b4d196ef3bf77e4279642f486967ce02da7f18e9
InSeyD <148376710+InSeyD@users.noreply.github.com>
2024-05-16 09:17:17 +0000

    Написал функционал в js для отзывов

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+//hamburger
 const hamburger = document.querySelector('#hamburger');
 const cross = document.querySelector('#cross');
 const hambMenu = document.querySelector('#hambmenu');
@@ -9,5 +10,45 @@ hamburger.addEventListener('click', function(){
 
 cross.addEventListener('click', function(){
     document.getElementsByClassName('hamburger--menu')[0].style.display = "none";;
+})
+
+//reviews
+const avatarOne = document.querySelector('#avatar-1');
+const avatarTwo = document.querySelector('#avatar-2');
+const avatarThree = document.querySelector('#avatar-3');
+
+const reviewOne = document.querySelector('#review-1');
+const reviewTwo = document.querySelector('#review-2');
+const reviewThree = document.querySelector('#review-3');
+
+
+avatarOne.addEventListener('click', function(){
+    reviewOne.style.display = "flex";
+    reviewTwo.style.display = "none";
+    reviewThree.style.display = "none";
+
+    avatarOne.classList.add('interactive-avatar--active');
+    avatarTwo.classList.remove('interactive-avatar--active');
+    avatarThree.classList.remove('interactive-avatar--active');
+})
+
+avatarTwo.addEventListener('click', function(){
+    reviewOne.style.display = "none";
+    reviewTwo.style.display = "flex";
+    reviewThree.style.display = "none";
+
+    avatarOne.classList.remove('interactive-avatar--active');
+    avatarTwo.classList.add('interactive-avatar--active');
+    avatarThree.classList.remove('interactive-avatar--active');
+})
+
+avatarThree.addEventListener('click', function(){
+    reviewOne.style.display = "none";
+    reviewTwo.style.display = "none";
+    reviewThree.style.display = "flex";
+
+    avatarOne.classList.remove('interactive-avatar--active');
+    avatarTwo.classList.remove('interactive-avatar--active');
+    avatarThree.classList.add('interactive-avatar--active');
 })
 
